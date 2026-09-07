66acbdb18a9552e3b07bfca9b936976f27c56f79
r0xx3z <axelgpg2290@gmail.com>
2026-09-06 21:47:41 -0500

    REFACTOR: device tree reconfigured

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,68 +1,64 @@
+# Copyright (C) 2025-2026 OrangeFox Recovery Project
+# SPDX-License-Identifier: GPL-3.0-only
+
+FDEVICE="avalon"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep -w $FDEVICE)
+   if [ -n "$chkdev" ]; then 
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep -w $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+
+# Build
+export FOX_USE_SPECIFIC_MAGISK_ZIP=~/Magisk/Magisk-v30.7.zip
+export TW_DEFAULT_LANGUAGE="en"
+export LC_ALL="C"
 export ALLOW_MISSING_DEPENDENCIES=true
 
-# Some about us
-export FOX_MAINTAINER_PATCH_VERSION=0
-export OF_MAINTAINER="Sathiya_Senpai"
-
 # Build environment stuffs
-export FOX_BUILD_DEVICE="Nord 4"
 export TARGET_DEVICE_ALT="avalon, OP5E93L1"
 export FOX_TARGET_DEVICES="avalon, OP5E93L1"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
-export FOX_BUILD_TYPE="Beta"
-export FOX_VANILLA_BUILD=1
 
-# We have a/b partitions
+# A/B partitions
 export FOX_AB_DEVICE=1
 export FOX_VIRTUAL_AB_DEVICE=1
 export OF_AB_DEVICE_WITH_RECOVERY_PARTITION=1
 
-# Screen specifications
-export OF_STATUS_INDENT_LEFT=48
-export OF_STATUS_INDENT_RIGHT=48
-export OF_ALLOW_DISABLE_NAVBAR=0
-export OF_HIDE_NOTCH=1
-export OF_CLOCK_POS=1
-export OF_SCREEN_H=2376
-export OF_STATUS_H=120
-export OF_USE_GREEN_LED=0
-export OF_OPTIONS_LIST_NUM=8
-
 # Device stuff
 export OF_DEFAULT_KEYMASTER_VERSION=4.1
-export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
-export OF_FBE_METADATA_MOUNT_IGNORE=1
-export OF_WIPE_METADATA_AFTER_DATAFORMAT=1
-export OF_USE_LEGACY_BATTERY_SERVICES=1
 export OF_FORCE_PREBUILT_KERNEL=1
 export OF_DYNAMIC_FULL_SIZE=9126805504
 
-# OTA for custom ROMs
-export OF_SUPPORT_ALL_PAYLOAD_OTA_UPDATES=1
-export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
-
-# Add some extras
-export FOX_USE_UPDATED_MAGISKBOOT=1
-export FOX_USE_ZSTD_BINARY=1
+# Binaries & Tools
+export OF_ADVANCED_SECURITY=1
+export FOX_BASH_TO_SYSTEM_BIN=1
+export FOX_USE_LZ4_COMPRESSION=1
 export FOX_USE_ZIP_BINARY=1
-export FOX_USE_XZ_UTILS=1
+export FOX_USE_BUSYBOX_BINARY=1
+export FOX_USE_NANO_EDITOR=1
 export FOX_USE_TAR_BINARY=1
 export FOX_USE_SED_BINARY=1
-export FOX_USE_NANO_EDITOR=1
-export FOX_USE_GREP_BINARY=1
-export FOX_USE_BUSYBOX_BINARY=1
+export FOX_USE_XZ_UTILS=1
 export FOX_USE_BASH_SHELL=1
-export FOX_BASH_TO_SYSTEM_BIN=1
-export OF_USE_LZ4_COMPRESSION=1
-export FOX_REPLACE_TOOLBOX_GETPROP=1
-export OF_ENABLE_LPTOOLS=1
-export FOX_ASH_IS_BASH=1
 export FOX_DELETE_AROMAFM=1
-export OF_ENABLE_ALL_PARTITION_TOOLS=1
-export OF_NO_ADDITIONAL_MIUI_PROPS_CHECK=1
-export OF_QUICK_BACKUP_LIST="/boot;/dtbo;"
-export OF_ADVANCED_SECURITY=1
-export FOX_INSTALLER_DEBUG_MODE=1
-export FOX_ENABLE_APP_MANAGER=1
-export FOX_INSTALLER_DISABLE_AUTOREBOOT=1
-export OF_RUN_POST_FORMAT_PROCESS=1
+
+# Maintainer & Build Info
+export FOX_BUILD_TYPE="Testing"
+export FOX_MAINTAINER_PATCH_VERSION="3"
+export OF_MAINTAINER="Sathiya_Senpai"
+	
+else
+   if [ -z "$FOX_BUILD_DEVICE" -a -z "$BASH_SOURCE" ]; then
+      echo "I: This script requires bash. Not processing the $FDEVICE $(basename $0)"
+   fi
+fi
